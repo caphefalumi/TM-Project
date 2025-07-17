275e3f02fd821e5c36b51c0268e78bff9c4187f3
Mingsy <khanhkelvin08122006@gmail.com>
2025-07-17 14:07:06 +0700

    Fix bug

---
+++ src/client/views/AdminView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const user = ref({
+  userId: '',
+  username: '',
+  email: '',
+})
+
+const isAdmin = ref(false)
+
+const props = defineProps({
+    userProps: {
+        type: Object,
+        required: true,
+    },
+})
+
+</script>
+
+<template>
+    
+</template>
