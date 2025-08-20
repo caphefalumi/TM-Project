6eb24309e9290bd9c3d82c8f1b295b26a1bf9828
Hieu Phan <hphan5283@gmail.com>
2025-08-20 16:55:18 +0700

    api refactor

---
+++ server/routes/announcements_routes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import Announcements from '../api/announcements.js'
+
+const router = express.Router()
+const { toggleLikeAnnouncement, addCommentToAnnouncement } = Announcements
+
+// Likes & Comments
+router.post('/:announcementId/like', toggleLikeAnnouncement)
+router.post('/:announcementId/comments', addCommentToAnnouncement)
+
+export default router
