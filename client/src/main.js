df808a338d45acafcde32b2ba4c8011f72ed51e8
caphefalumi <dangduytoan13l@gmail.com>
2025-09-20 18:53:02 +0700

    refactor: remove Pinia state management from project

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createPinia } from 'pinia'
 
 import 'vuetify/styles'
 import vuetify from './plugins/vuetify.js' // Import the configured instance
@@ -11,7 +10,6 @@ import vue3GoogleLogin from 'vue3-google-login'
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
 
 const app = createApp(App)
-const pinia = createPinia()
 
 app.use(vue3GoogleLogin, {
   clientId: CLIENT_ID,
@@ -19,5 +17,4 @@ app.use(vue3GoogleLogin, {
 
 app.use(router)
 app.use(vuetify)
-app.use(pinia)
 app.mount('#app')
