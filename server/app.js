99abbe2a58936cdbf4919f7f88b6d31c81dbc4aa
caphefalumi <dangduytoan13l@gmail.com>
2025-10-04 20:51:40 +0700

    chore: update tauri localhost to cors

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ app.use(
       'http://localhost:5173',
       'http://localhost:5174',
       'https://tm-project-weld.vercel.app',
+  		"http://tauri.localhost",
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
