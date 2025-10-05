220df246f617af6ec6f8219c098d39405bf7ae3c
caphefalumi <dangduytoan13l@gmail.com>
2025-10-05 22:44:15 +0700

    fix: vercel build

--- server/app.js
+++ server/app.js
@@ -43,6 +43,10 @@ app.use((req, _res, next) => {
 
   next()
 })
+app.use((req, res, next) => {
+  console.log('Incoming request origin:', req.headers.origin);
+  next();
+})
 // Increase payload size limit for image uploads
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
